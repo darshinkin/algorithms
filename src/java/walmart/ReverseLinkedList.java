881db4661a3e50e59486547498603541f15acebb
Dmitry Arshinkin <arshinkinda@gmail.com>
2022-05-14 13:15:27 +0300

    Reverse LinkedList

---
+++ src/java/walmart/ReverseLinkedList.java
@@ -0,0 +1,47 @@
+package walmart;
+
+public class ReverseLinkedList {
+
+    static class Node {
+        Character value;
+        Node nextNode;
+
+        public Node(Character value, Node nextNode) {
+            this.value = value;
+            this.nextNode = nextNode;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node nodeD = new Node('d', null);
+        Node nodeC = new Node('c', nodeD);
+        Node nodeB = new Node('b', nodeC);
+        Node nodeA = new Node('a', nodeB);
+        Node result = new ReverseLinkedList().reverse(nodeA);
+        while (true) {
+            System.out.print(result.value + " -> ");
+            result = result.nextNode;
+            if (result == null) {
+                System.out.println("null");
+                break;
+            }
+        }
+    }
+
+    private Node reverse(Node head) {
+        Node previous = null;
+        Node current = head;
+        Node next;
+        while (true) {
+            next = current.nextNode;
+            current.nextNode = previous;
+            if (next == null) {
+                break;
+            }
+            previous = current;
+            current = next;
+        }
+
+        return current;
+    }
+}
